e8e03b505b4e2359cd10644bcdfa956151a5a2df
ndukauche <115647671+ndukauche@users.noreply.github.com>
2023-01-15 18:10:34 +0000

    Add files via upload

---
+++ credit card regx.js
@@ -0,0 +1,9 @@
+// A program that will validate a credit-card   
+// will be checking 
+// bank
+// name on card 
+// password 
+// number on card
+const card = /\S\S\s\d{10,12}\s\d{3,}/.test("American-express Nduka-Uchenna 12348512345 345");
+console.log(card)
+
